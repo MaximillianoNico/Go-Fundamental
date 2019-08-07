10d1397a372e8de7975aaec076de5e6c0790909f
MaximillianoNico <maximillianonico8@gmail.com>
2019-08-07 18:20:09 +0700

    material Variable, Type data and Hello world

---
+++ TypeData/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var numberUint8 uint8 = 10 // uint8 0 ↔ 255
+	fmt.Println(numberUint8)
+}
---
+++ Variable/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var username string = "admin"
+	var fullname string = "localhost"
+
+	lastname := "abc"
+
+	fmt.Printf("Data %s %s %s", username, fullname, lastname)
+}
